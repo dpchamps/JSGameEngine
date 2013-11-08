8d8c66269f8d4a87dc57e7dd6c66d714a2f82f1a
Dave <gasolinewaltz@gmail.com>
2013-11-07 16:12:02 -0800

    script/index.js
    Ideally, browserify should create the bundle from here, then all that needs to be required is 'bundle.js'. We'll see if that's how it works... It's a learning project.

---
+++ script/index.js
@@ -0,0 +1,15 @@
+require('vendor/underscore.js');
+require('vendor/eveline.js');
+require('vendor/canvasquery.js');
+
+require('engine/engine.js');
+require('engine/loader.js');
+require('engine/assets.js');
+
+require('engine/application.js');
+require('engine/scene.js');
+require('engine/collection.js');
+
+require('engine/main.js');
+require('engine/entities.js');
+require('engine/game.js');
